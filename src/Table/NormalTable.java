4c5506db401d8e131dea1aa579edd7328f7fa350
theminer3746 <me@theminerdev.com>
2018-12-05 17:59:21 +0700

    Create NormalTable class

---
+++ src/Table/NormalTable.java
@@ -0,0 +1,15 @@
+package Table;
+
+public class NormalTable extends Table {
+
+	@Override
+	public float getPrice() {
+		return 50;
+	}
+
+	@Override
+	public String getName() {
+		return "Normal Table Service Charge";
+	}
+
+}
