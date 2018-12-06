1841bd6f71c1d2b318191b9c62f6b954189203d4
theminer3746 <me@theminerdev.com>
2018-12-07 01:39:19 +0700

    Remove redundant code

--- src/Table/NormalTable.java
+++ src/Table/NormalTable.java
@@ -2,7 +2,6 @@ package Table;
 
 public class NormalTable extends Table {
 	private final double SERVICE_CHARGE_RATE = 0.05;
-	protected int maximumGuest = 6;
 
 	@Override
 	public double getPrice() {
