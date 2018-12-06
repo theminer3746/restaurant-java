231fafe91a4ed167654fc819501b0a07adf1b3fc
Rapii Zeta <infinitereload@gmail.com>
2018-12-07 01:53:02 +0700

    Add VBox to Main and create Lanch "Test push"

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,29 @@
 package application;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
 	@Override
-	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
+	public void start(Stage primaryStage) throws Exception {
+		VBox root = new VBox();
+		Scene scene = new Scene(root);
+		//scene.getStylesheets().add("ProgMeth.css");
+		root.setPadding(new Insets(5));
+		root.setSpacing(5);
+		root.setMinWidth(250);
+		root.setMinHeight(400);
+		
+		primaryStage.setTitle("Restaurant");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
 	}
 }
