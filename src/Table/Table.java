19a046f3b7d6ce93baa64f379dae3a5bdfd5d280
theminer3746 <me@theminerdev.com>
2018-12-06 22:31:09 +0700

    Implement logic concerning guest amount

--- src/Table/Table.java
+++ src/Table/Table.java
@@ -2,11 +2,13 @@ package Table;
 
 import java.util.ArrayList;
 
+import Exception.NotEnoughSeatException;
 import logic.Billable;
 
 public abstract class Table implements Billable {
 	protected ArrayList<Billable> billable;
 	protected int guestAmount;
+	protected int maximumGuest;
 	
 	public double getBillableTotal() {
 		double total = 0;
@@ -34,4 +36,24 @@ public abstract class Table implements Billable {
 	public abstract double getPrice();
 
 	public abstract String getName();
+
+	public int getGuestAmount() {
+		return guestAmount;
+	}
+
+	public void setGuestAmount(int guestAmount) throws NotEnoughSeatException {
+		if(guestAmount > maximumGuest) {
+			throw new NotEnoughSeatException();
+		}
+		
+		this.guestAmount = guestAmount;
+	}
+
+	public int getMaximumGuest() {
+		return maximumGuest;
+	}
+
+	public void setMaximumGuest(int maximumGuest) {
+		this.maximumGuest = maximumGuest;
+	}
 }
