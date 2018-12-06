8854433997293005e940b600441fa280f1dcac03
theminer3746 <me@theminerdev.com>
2018-12-07 00:56:29 +0700

    Create Bill class

---
+++ src/logic/Bill.java
@@ -0,0 +1,21 @@
+package logic;
+
+import java.util.ArrayList;
+
+public class Bill {
+	private ArrayList<Billable> billable;
+	
+	public void addToBill(Billable billable) {
+		this.billable.add(billable);
+	}
+	
+	public double getBillableTotal() {
+		double total = 0;
+		
+		for(Billable item : this.billable) {
+			total += item.getPrice();
+		}
+		
+		return total;
+	}
+}
