e3396c01389020f108b53446b69b9c68847b6e4c
theminer3746 <me@theminerdev.com>
2018-12-08 01:03:11 +0700

    Create RestaurantManagement class

---
+++ src/logic/RestaurantManagement.java
@@ -0,0 +1,23 @@
+package logic;
+
+import java.util.ArrayList;
+
+import Table.Table;
+
+public class RestaurantManagement {
+	private ArrayList<Table> tables;
+	private Menus menus;
+
+	public RestaurantManagement() {
+		populateTable();
+		populateMenus();
+	}
+	
+	private void populateTable() {
+		
+	}
+	
+	private void populateMenus() {
+		
+	}
+}
