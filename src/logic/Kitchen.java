ac8e94a7016f42953d8dcf810ec1e944c9258c13
theminer3746 <me@theminerdev.com>
2018-12-08 18:08:28 +0700

    Make Kitchen class a singleton

--- src/logic/Kitchen.java
+++ src/logic/Kitchen.java
@@ -1,5 +1,18 @@
 package logic;
 
 public class Kitchen {
-
+	private OrderList orderList;
+	private static Kitchen instance;
+	
+	private Kitchen() {
+		this.orderList = new OrderList();
+	}
+	
+	public static Kitchen getInstance() {
+		if(instance == null) {
+			instance = new Kitchen();
+		}
+		
+		return instance;
+	}
 }
