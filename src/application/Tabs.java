e198aa73bef4c91e8171f0fa9d61606fa942576c
Rapii Zeta <infinitereload@gmail.com>
2018-12-08 08:16:54 +0700

    Change from setVisibility method to switch between tabs to add, remove.

--- src/application/Tabs.java
+++ src/application/Tabs.java
@@ -2,24 +2,45 @@ package application;
 
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class Tabs extends HBox {
 	
-	public Tabs(TableGrid tableGrid) {
+	public Tabs(VBox root, TableGrid tableGrid, Kitchen kitchen) {
 		setSpacing(5);
 
 		
 		Button counterBtn = new Button("Counter");
 		//counterBtn.getStyleClass().add("Table");
 		counterBtn.setOnAction(e -> {
-			tableGrid.setVisible(true);
-			/////
+			boolean gridStatus = tableGrid.getStatus();
+			boolean kitchenStatus = kitchen.getStatus();
+			if (gridStatus == true) {
+				
+			} else if (gridStatus == false && kitchenStatus == false) {
+				root.getChildren().add(tableGrid);
+			} else {
+				root.getChildren().remove(kitchen);
+				root.getChildren().add(tableGrid);
+			}
+			tableGrid.setStatus(true);
+			kitchen.setStatus(false);
 		});
 		
 		Button kitchenBtn = new Button("Kitchen");
 		kitchenBtn.setOnAction(e -> {
-			tableGrid.setVisible(false);
-			/////
+			boolean gridStatus = tableGrid.getStatus();
+			boolean kitchenStatus = kitchen.getStatus();
+			if (kitchenStatus == true) {
+				
+			} else if (gridStatus == false && kitchenStatus == false) {
+				root.getChildren().add(kitchen);
+			} else {
+				root.getChildren().remove(tableGrid);
+				root.getChildren().add(kitchen);
+			}
+			tableGrid.setStatus(false);
+			kitchen.setStatus(true);
 		});
 		
 		Button closeBtn = new Button("Close");
