fc5257449eeebfa7874c2f599eb95abe5107a7cf
Rapii Zeta <infinitereload@gmail.com>
2018-12-08 20:58:12 +0700

    Beautified Main Class.(?)

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,58 +12,58 @@ import javafx.stage.StageStyle;
 import logic.Restaurant;
 
 public class Main extends Application {
-	
-	//define offset
+
+	// define offset
 	private double xOffset = 0;
 	private double yOffset = 0;
-	
+
 	TableGrid tableGrid;
 	KitchenPane kitchen;
-	
+
 	Restaurant restaurant;
-	
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
 		restaurant = Restaurant.getInstance();
+		
 		tableGrid = new TableGrid(restaurant.getTableList());
 		kitchen = new KitchenPane();
-		
+
 		primaryStage.initStyle(StageStyle.TRANSPARENT);
-		
+
 		VBox root = new VBox();
 		root.setOnMousePressed(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                xOffset = event.getSceneX();
-                yOffset = event.getSceneY();
-            }
+			@Override
+			public void handle(MouseEvent event) {
+				xOffset = event.getSceneX();
+				yOffset = event.getSceneY();
+			}
 		});
-		
-		 root.setOnMouseDragged(new EventHandler<MouseEvent>() {
-	            @Override
-	            public void handle(MouseEvent event) {
-	                primaryStage.setX(event.getScreenX() - xOffset);
-	                primaryStage.setY(event.getScreenY() - yOffset);
-	            }
-	    });
-		
+
+		root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				primaryStage.setX(event.getScreenX() - xOffset);
+				primaryStage.setY(event.getScreenY() - yOffset);
+			}
+		});
+
 		Scene scene = new Scene(root);
 		scene.getStylesheets().add("Restaurant.css");
 		root.setPadding(new Insets(5));
 		root.setSpacing(5);
 		root.setMinWidth(750);
 		root.setMinHeight(400);
-		
+
 		Tabs tabs = new Tabs(root, tableGrid, kitchen);
 		root.getChildren().addAll(tabs);
-		
+
 		primaryStage.setTitle("Restaurant");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
