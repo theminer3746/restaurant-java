b5fbf024b649429997b9bc7e7d229aefd20b93e7
Rapii Zeta <infinitereload@gmail.com>
2018-12-08 20:59:06 +0700

    Created ERROR the same way with TableGrid.

--- src/application/OrderInput.java
+++ src/application/OrderInput.java
@@ -5,23 +5,42 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.HBox;
-import logic.Order;
 
 public class OrderInput extends HBox  {
 	
 	
 	public OrderInput(Table table, String name) {
 		setAlignment(Pos.CENTER_LEFT);
-		setSpacing(150);
+		setSpacing(60);
 		Label orderName = new Label(name);
 		TextField quantity = new TextField();
 		quantity.setPromptText("Quantity");
 		Button order = new Button("Order");
 		order.setOnMouseClicked(e -> {
-			int orderQuantity = quantity.getText();
-			// create new order add to orderlist
+			try {
+				int amount = Integer.parseInt(quantity.getText());
+				if (amount <= 0) {
+					Notify notify = new Notify(AlertType.ERROR);
+					notify.showNegativeQuantityError();
+					
+				} else {
+					// create new order add to orderlist
+					/// place order
+
+				}
+			} catch (NumberFormatException e1) {
+				if (quantity.getText().isEmpty()) {
+					Notify notify = new Notify(AlertType.ERROR);
+					notify.showEmptyQuantityTextFieldError();
+					
+				} else {
+					Notify notify = new Notify(AlertType.ERROR);
+					notify.showIncorrectQuantityFormatError();
+				}
+			}
 		});
-		getChildren().addAll(orderName, order);
+		getChildren().addAll(orderName, quantity, order);
 	}
 }
