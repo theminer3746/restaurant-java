822aba38f8d1dcd815a976fd973a1166b7252f72
theminer3746 <me@theminerdev.com>
2018-12-08 18:23:46 +0700

    Create TableList wrapper class

---
+++ src/Table/TableList.java
@@ -0,0 +1,41 @@
+package Table;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+import jdk.nashorn.internal.parser.JSONParser;
+
+public class TableList {
+	private static TableList instance;
+	private ArrayList<Table> tables;
+	
+	private TableList() {
+		populateTableData();
+	}
+	
+	public static TableList getInstance() {
+		if(instance == null) {
+			instance = new TableList();
+		}
+		
+		return instance;
+	}
+	
+	private void populateTableData() {
+		//JSONArray arr = new JSONArray();
+		
+		try {
+			String uri = ClassLoader.getSystemResource("tables.json").toString();
+			FileReader fileReader = new FileReader(uri);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void addTableToList(Table table) {
+		//TODO
+		this.tables.add(table);
+	}
+}
