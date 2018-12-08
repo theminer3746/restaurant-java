2e15cb200ad588ea44c2e13d3363c135bc1de278
Rapii Zeta <infinitereload@gmail.com>
2018-12-08 19:11:01 +0700

    Added quantity option.

--- src/application/OrderInput.java
+++ src/application/OrderInput.java
@@ -4,6 +4,7 @@ import Table.Table;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import logic.Order;
 
@@ -14,10 +15,12 @@ public class OrderInput extends HBox  {
 		setAlignment(Pos.CENTER_LEFT);
 		setSpacing(150);
 		Label orderName = new Label(name);
+		TextField quantity = new TextField();
+		quantity.setPromptText("Quantity");
 		Button order = new Button("Order");
 		order.setOnMouseClicked(e -> {
-			
-			//table.getBill().addToBill(new Order(null, table));
+			int orderQuantity = quantity.getText();
+			// create new order add to orderlist
 		});
 		getChildren().addAll(orderName, order);
 	}
