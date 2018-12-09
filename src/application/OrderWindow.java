5ba137be6928fe61e2c1481359ca7240bf5eca2a
Rapii Zeta <infinitereload@gmail.com>
2018-12-09 19:44:58 +0700

    Added ERROR for when there are/ is some order(s) of this table in the kitchen.

--- src/application/OrderWindow.java
+++ src/application/OrderWindow.java
@@ -1,18 +1,22 @@
 package application;
 
 import Table.Table;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import logic.Menu;
+import logic.Order;
 import logic.Restaurant;
 
 public class OrderWindow extends Stage {
-	public OrderWindow(Table table, Button tableBtn, Restaurant restaurant, KitchenPane kitchenPane) {
+	public OrderWindow(Table table, Button tableBtn, Restaurant restaurant, KitchenPane kitchenPane, ObservableList<Label> logServeList/**/) {
 		VBox orderWindow = new VBox();
 		orderWindow.setMinWidth(250);
 		orderWindow.setAlignment(Pos.TOP_CENTER);
@@ -22,15 +26,32 @@ public class OrderWindow extends Stage {
 
 		Label label = new Label("Menu List");
 		orderWindow.getChildren().add(label);
-		
-		 for (Menu menu : restaurant.getMenuList().getMenus()) {
-			 orderWindow.getChildren().add(new OrderInput(table, menu, restaurant.getKitchen().getOrderList(), kitchenPane));
-		 }
-		 
-		if (table.getBill().getBillableTotal() == 0) {
+
+		for (Menu menu : restaurant.getMenuList().getMenus()) {
+			orderWindow.getChildren()
+					.add(new OrderInput(table, menu, restaurant.getKitchen().getOrderList(), kitchenPane, logServeList /**/));
+		}
+
+		if (table.getBill().getBillableTotal() != 0) {
 			Button checkBtn = new Button("Check");
 			checkBtn.setOnMouseClicked(e1 -> {
-				new CheckWindow(table, tableBtn, this);
+
+				boolean found = false;
+				for (Order order : restaurant.getKitchen().getOrderList().getOrders()) {
+					if (order.getTable().getTableNumber() == table.getTableNumber()) {
+						found = true;
+					}
+				}
+				if (found == true) {
+					Alert alert = new Alert(AlertType.ERROR);
+					alert.setTitle("Error");
+					alert.setHeaderText("Error");
+					alert.setContentText("Some of this table's order(s) is still being prepared in the kitchen");
+					alert.show();
+				} else {
+					new CheckWindow(table, tableBtn, this, logServeList);
+				}
+				
 			});
 			orderWindow.getChildren().add(checkBtn);
 		}
