e36bdab84590cf685e1216310469bf213f51b3ed
theminer3746 <me@theminerdev.com>
2018-12-09 18:35:58 +0700

    Beautify code

--- src/logic/MenuList.java
+++ src/logic/MenuList.java
@@ -5,26 +5,26 @@ import java.util.ArrayList;
 public class MenuList {
 	private static MenuList instance;
 	private ArrayList<Menu> menus;
-	
+
 	public MenuList() {
 		menus = new ArrayList<Menu>();
 		populateMenuList();
 	}
 
 	public static MenuList getInstance() {
-		if(instance == null) {
+		if (instance == null) {
 			instance = new MenuList();
 		}
-		
+
 		return instance;
 	}
-	
+
 	private void populateMenuList() {
 		menus.add(new Menu("Carbonara spaghetti", 150));
 		menus.add(new Menu("Hawaiian pizza", 200));
 		menus.add(new Menu("Meatball", 175));
 	}
-	
+
 	public void addMenu(Menu menu) {
 		menus.add(menu);
 	}
