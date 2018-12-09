023f6697cc5a6c4c69b87207916146b00cd19fa4
theminer3746 <me@theminerdev.com>
2018-12-09 16:07:33 +0700

    Make MenuList a singleton

--- src/logic/MenuList.java
+++ src/logic/MenuList.java
@@ -3,12 +3,21 @@ package logic;
 import java.util.ArrayList;
 
 public class MenuList {
+	private static MenuList instance;
 	private ArrayList<Menu> menus;
-
+	
 	public MenuList() {
 		menus = new ArrayList<Menu>();
 	}
 
+	public static MenuList getInstance() {
+		if(instance == null) {
+			instance = new MenuList();
+		}
+		
+		return instance;
+	}
+	
 	public void addMenu(Menu menu) {
 		menus.add(menu);
 	}
