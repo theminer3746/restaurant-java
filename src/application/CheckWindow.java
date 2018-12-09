08b9f2a7a1d26d3a75c6405124d99db0fc9d9d72
Rapii Zeta <infinitereload@gmail.com>
2018-12-09 19:43:24 +0700

    Move ObservationList to tableGrid and rename it LogServeList.

--- src/application/CheckWindow.java
+++ src/application/CheckWindow.java
@@ -24,23 +24,23 @@ import logic.Bill;
 
 public class CheckWindow extends Stage {
 	
-	/*private ObservableList<Label> logOrdersList = FXCollections.observableArrayList();
-	private ListView<Label> logListView;*/
+	//private ObservableList<Label> logOrdersList = FXCollections.observableArrayList();
+	private ListView<Label> logListView;
 	
-	public CheckWindow(Table table, Button tableBtn, Stage stage) {
+	public CheckWindow(Table table, Button tableBtn, Stage stage, ObservableList<Label> logServeList) {
 		VBox checkWindow = new VBox();
 		Scene checkScene = new Scene(checkWindow);
 		checkWindow.setSpacing(5);
 		checkWindow.setPadding(new Insets(5));
 		checkWindow.setAlignment(Pos.CENTER_RIGHT);
 
-		/*logListView = new ListView<Label>(logOrdersList);
+		logListView = new ListView<Label>(logServeList);
 		logListView.setPrefWidth(250);
 		logListView.setFocusTraversable(false);
 		logListView.setBackground(new Background(new BackgroundFill(Color.IVORY, null, null)));
 		logListView.setPlaceholder(new Label("No Orders"));
 		logListView.setBorder(new Border(new BorderStroke(Color.BLUE, BorderStrokeStyle.SOLID, 
-				CornerRadii.EMPTY, BorderWidths.DEFAULT)));*/
+				CornerRadii.EMPTY, BorderWidths.DEFAULT)));
 		
 		/*
 		 * 
@@ -57,6 +57,7 @@ public class CheckWindow extends Stage {
 				table.setGuestAmount(0);
 				tableBtn.getStyleClass().clear();
 				tableBtn.getStyleClass().add(table.getClass().getSimpleName());
+				logServeList.clear();
 				close();
 				stage.close();
 			} catch (NotEnoughSeatException e3) {
@@ -68,7 +69,7 @@ public class CheckWindow extends Stage {
 		cancel.setOnMouseClicked(e2 -> {
 			
 		});
-		checkWindow.getChildren().addAll(/*logListView,*/ serviceCharge, total, confirm);
+		checkWindow.getChildren().addAll(logListView, serviceCharge, total, confirm);
 
 		setTitle("Check");
 		setScene(checkScene);
